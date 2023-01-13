3a86edd8e37139c25541832ab1c1a71811c10cb1
Shawn Ngo <shawn@snappymob.com>
2023-01-14 07:50:03 +0800

    [NYTimes] Add AppConstant

---
+++ nytimes-ios/Utilities/AppConstant.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct AppConstants {
+    static let baseURL: URL = URL(string: "https://api.nytimes.com/svc/")!
+
+    static let appId = "a8f5bfe5-cde5-43d8-bc47-ac5584ed4b6c"
+
+    static let apiKey = "aBBpWBWdHveK5y5w9Cbln7kci0vNiT0g"
+}
