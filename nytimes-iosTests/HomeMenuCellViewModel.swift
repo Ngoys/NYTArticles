a9fecc7bc036d5b4d3d85603a29b5280bc640595
Shawn Ngo <shawn@snappymob.com>
2023-01-31 15:39:51 +0800

    [NYTimes] Add viewModel.isRightArrowImageViewHidden test

--- nytimes-iosTests/HomeMenuCellViewModel.swift
+++ nytimes-iosTests/HomeMenuCellViewModel.swift
@@ -28,30 +28,40 @@ class HomeMenuCellViewModelTest: BaseTest {
     // MARK: - Tests
     //----------------------------------------
 
-    func testMostEmailedHomeMenuTitleText() {
+    func testTitleText() {
         setupViewModel(homeMenu: .mostEmailed)
         XCTAssertEqual(viewModel.titleText, "Most Emailed")
-    }
 
-    func testMostSharedHomeMenuTitleText() {
         setupViewModel(homeMenu: .mostShared)
         XCTAssertEqual(viewModel.titleText, "Most Shared")
-    }
 
-    func testMostViewedHomeMenuTitleText() {
         setupViewModel(homeMenu: .mostViewed)
         XCTAssertEqual(viewModel.titleText, "Most Viewed")
-    }
 
-    func testSearchArticleHomeMenuTitleText() {
         setupViewModel(homeMenu: .searchArticle)
         XCTAssertEqual(viewModel.titleText, "Search Articles")
-    }
 
-    func testLocationHomeMenuTitleText() {
         let mockCLLocation = CLLocation(latitude: 10, longitude: -10)
         setupViewModel(homeMenu: .location(clLocation: mockCLLocation))
-        
+
         XCTAssertEqual(viewModel.titleText, "Latitude: \(mockCLLocation.coordinate.latitude)\nLongitude: \(mockCLLocation.coordinate.longitude)")
+    }
+
+    func testIsRightArrowImageViewHidden() {
+        setupViewModel(homeMenu: .mostEmailed)
+        XCTAssertEqual(viewModel.isRightArrowImageViewHidden, false)
+
+        setupViewModel(homeMenu: .mostShared)
+        XCTAssertEqual(viewModel.isRightArrowImageViewHidden, false)
+
+        setupViewModel(homeMenu: .mostViewed)
+        XCTAssertEqual(viewModel.isRightArrowImageViewHidden, false)
+
+        setupViewModel(homeMenu: .searchArticle)
+        XCTAssertEqual(viewModel.isRightArrowImageViewHidden, false)
+
+        let mockCLLocation = CLLocation(latitude: 10, longitude: -10)
+        setupViewModel(homeMenu: .location(clLocation: mockCLLocation))
+        XCTAssertEqual(viewModel.isRightArrowImageViewHidden, true)
     }
 }
